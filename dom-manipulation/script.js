cd97f6ebe2f3cfb10994fe93ad1a0510c2527092
Nonhlanhla Mokoena <mokoena3586@gmail.com>
2024-10-26 16:34:59 +0200

    Add script.js

---
+++ dom-manipulation/script.js
@@ -0,0 +1,30 @@
+let quotes = [
+    { text: "The only limit to our realization of tomorrow is our doubts of today.", category: "Inspiration" },
+    { text: "Do not wait to strike till the iron is hot; but make it hot by striking.", category: "Motivation" },
+  ];
+
+
+
+  function showRandomQuote() {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    const quote = quotes[randomIndex];
+    document.getElementById("quoteDisplay").innerText = `"${quote.text}" - ${quote.category}`;
+  }
+  
+  document.getElementById("newQuote").addEventListener("click", showRandomQuote);
+
+
+
+  function addQuote() {
+    const newQuoteText = document.getElementById("newQuoteText").value;
+    const newQuoteCategory = document.getElementById("newQuoteCategory").value;
+  
+    if (newQuoteText && newQuoteCategory) {
+      quotes.push({ text: newQuoteText, category: newQuoteCategory });
+      document.getElementById("newQuoteText").value = "";
+      document.getElementById("newQuoteCategory").value = "";
+      alert("Quote added successfully!");
+    } else {
+      alert("Please enter both quote text and category.");
+    }
+  }
